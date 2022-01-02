231337f6aa91b48530273f52a23a2bf079ef0e55
Jan Dolejš <jandolejs1999@gmail.com>
2022-01-02 11:54:12 +0100

    add tracy

--- src/app/Bootstrap.php
+++ src/app/Bootstrap.php
@@ -14,7 +14,7 @@ class Bootstrap
         $configurator = new Configurator;
         $appDir       = dirname(__DIR__);
 
-        $configurator->setDebugMode(TRUE);
+        $configurator->setDebugMode( str_ends_with($_SERVER['HTTP_HOST'] ?? '', ".localhost"));
         $configurator->enableTracy($appDir . '/log');
 
         $configurator->setTimeZone('Europe/Prague');
